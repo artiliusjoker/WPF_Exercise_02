0517cd001a7ec24e26ffc28ba30440423e60f416
Minh Quan <1712695@student.hcmus.edu.vn>
2020-10-10 00:51:05 +0700

    finish display questions on screen

--- WpfApp/MainWindow.xaml.cs
+++ WpfApp/MainWindow.xaml.cs
@@ -1,18 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
+using System.IO;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WpfApp
 {
@@ -21,17 +12,80 @@ namespace WpfApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly string CURR_FOLDER = AppDomain.CurrentDomain.BaseDirectory;
+        private readonly List<Question> questions = new List<Question>();
+
+        // Danh cho cac thao tac random
+        private readonly List<int> chosenIndex = new List<int>();
+        private readonly Random _random = new Random();
+
         public MainWindow()
         {
+
             InitializeComponent();
-            string currFolder = AppDomain.CurrentDomain.BaseDirectory;
-            string picLocation = currFolder + "vietnam.png";
-            BitmapImage bitmapImage = new BitmapImage(new Uri(picLocation, UriKind.Absolute));
-            picture1.Source = bitmapImage;
-            picLocation = currFolder + "argentina.png";
-            bitmapImage = new BitmapImage(new Uri(picLocation, UriKind.Absolute));
-            picture2.Source = bitmapImage;
-            question.Content = "asdasd";
+            InitializeGame();
+        }
+    private void InitializeQuestionList() {
+            string[] lines = File.ReadAllLines(CURR_FOLDER + "AppData.txt");
+            
+            for(int i = 0; i < 10; i++)
+            {             
+                // Chon ngau nhien cau hoi
+                string line = lines[RandomizeLine(lines)];
+                string[] temp = line.Split(':');
+                Question question = new Question(temp[0], temp[1]);
+                // Chon ngau nhien cau tra loi sai de danh lac huong
+                line = lines[RandomizeLine(lines)];
+                temp = line.Split(':');
+                question.WrongAnswer = temp[1];
+                // Them cau hoi vao bo cau hoi
+
+                question.ConvertToAbsolutePath(CURR_FOLDER);
+                questions.Add(question);
+            }
+        }
+        private int RandomizeLine(string[] lines)
+        {
+            int randomLine = _random.Next(lines.Length);
+            while (chosenIndex.Exists(x => x == randomLine))
+            {
+                randomLine = _random.Next(lines.Length);
+            }
+            return randomLine;
+        }
+        private void InitializeGame() 
+        {
+            InitializeQuestionList();
+            ShowQuestionOnScreen(questions[0]);
+        }
+        private void ShowQuestionOnScreen(Question question)
+        {
+            questionOnScreen.Content = question.QuestionText;
+            bool flag = true;                   
+            BitmapImage bitmapImage = new BitmapImage(new Uri(question.CorrectAnswer, UriKind.Absolute));
+        
+            int randomizedPicturePosition = _random.Next(2);
+            // Dat vao cau tra loi dung
+            if(randomizedPicturePosition == 0)
+            {
+                picture1.Source = bitmapImage;              
+            }
+            else if (randomizedPicturePosition == 1)
+            {
+                flag = false;
+                picture2.Source = bitmapImage;              
+            }
+            // Dat vao cau tra loi sai
+            bitmapImage = new BitmapImage(new Uri(question.WrongAnswer, UriKind.Absolute));
+            if(flag)
+            {
+                picture2.Source = bitmapImage;
+            }
+            else if(!flag)
+            {
+                picture1.Source = bitmapImage;
+            }
         }
     }
+   
 }
---
+++ WpfApp/Question.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace WpfApp
+{
+    class Question
+    {
+        public string QuestionText { get; set; }
+        public string CorrectAnswer { get; set; }
+        public string WrongAnswer{get; set;}
+        // Khoi tao cau tra loi
+        public Question(string countryName, string correctAnswer)
+        {
+            StringBuilder questionBuilder = new StringBuilder();
+            questionBuilder.Append("Which is the flag of ");
+            questionBuilder.Append(countryName);
+            questionBuilder.Append(" ?");
+            QuestionText = questionBuilder.ToString();
+            CorrectAnswer = correctAnswer;
+        }
+        // Thiet lap cau tra loi sai
+
+        // Kiem tra cau tra loi dung
+        public bool IsCorrect(string answer)
+        {
+            int compareResult = CorrectAnswer.CompareTo(answer);
+            return (compareResult == 0);
+        }
+        public void ConvertToAbsolutePath(string currFolder)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append(currFolder);
+            stringBuilder.Append("\\assets\\");
+            stringBuilder.Append(CorrectAnswer);
+            CorrectAnswer = stringBuilder.ToString();
+
+            stringBuilder = new StringBuilder();
+            stringBuilder.Append(currFolder);
+            stringBuilder.Append("\\assets\\");
+            stringBuilder.Append(WrongAnswer);
+            WrongAnswer = stringBuilder.ToString();
+        }
+        public override string ToString()
+        {
+            return QuestionText;
+        }
+    }
+}
