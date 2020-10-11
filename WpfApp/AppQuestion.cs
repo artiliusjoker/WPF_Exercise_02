a4152607b8ab07a2f8ebde4c958b292a20fc2f42
Minh Quan <1712695@student.hcmus.edu.vn>
2020-10-11 17:47:48 +0700

    add AppQuestion

---
+++ WpfApp/AppQuestion.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp
+{
+    class AppQuestion
+    {
+        // First Question la hinh nam trong button thu 1
+        // Second Question la hinh nam trong button thu 2
+        public Question FirstQuestion { get; set; }
+        public Question SecondQuestion { get; set; }
+        public Question CorrectQuestion { get; set; }
+        public bool IsAnswered { get; set; }
+        public int TimeToAnswer { get; set; }
+        public AppQuestion()
+        {          
+            TimeToAnswer = 30;
+            IsAnswered = false;
+        }
+    }
+}
